2c6177c70aee7f40db0c78b5826c4b649642dba5
Max-Thunderbolt <maxvanderwalt3@gmail.com>
2025-06-24 10:49:22 +0200

    Created Empty ASP.NET C# MVC Project

---
+++ SITMS/SITMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SITMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
